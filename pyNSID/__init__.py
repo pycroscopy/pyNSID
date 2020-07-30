3865318362a019317d54fac0990dd1a919ebd9b2
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-07-30 17:19:59 -0400

    Corrected and simplified init

--- pyNSID/__init__.py
+++ pyNSID/__init__.py
@@ -1,7 +1,7 @@
-from . import io
-from .io import *
-from . import processing
-from .processing import *
+from . import io, processing, viz
+from .__version__ import version as __version__
 
 __all__ = ['__version__']
 __all__ += io.__all__
+__all__ += processing.__all__
+__all__ += viz.__all__
