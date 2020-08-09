4c3ceda100933b12e21a6373b92453ac60fc503a
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-09 17:31:42 -0400

    Cleaned up nsi_data a tiny bit

--- pyNSID/io/nsi_data.py
+++ pyNSID/io/nsi_data.py
@@ -243,196 +243,66 @@ class NSIDataset(h5py.Dataset):
             else:
                 raise NotImplementedError
 
+    def __validate_slice_dict(self, slice_dict):
+        """
+        Validates the slice dictionary
 
+        Parameters
+        ----------
+        slice_dict : dict
+            Dictionary of array-likes.
 
-def __validate_slice_dict(self, slice_dict):
-    """
-    Validates the slice dictionary
+        Returns
+        -------
+        None
+        """
+        if not isinstance(slice_dict, dict):
+            raise TypeError('slice_dict should be a dictionary of slice objects')
+        for key, val in slice_dict.items():
+            # Make sure the dimension is valid
+            if key not in self.n_dim_labels:
+                raise KeyError('Cannot slice on dimension {}.  '
+                               'Valid dimensions are {}.'.format(key, self.n_dim_labels))
+            if not isinstance(val, (slice, list, np.ndarray, tuple, int)):
+                raise TypeError('The slices must be array-likes or slice objects.')
+        return True
 
-    Parameters
-    ----------
-    slice_dict : dict
-        Dictionary of array-likes.
+    def slice(self, slice_dict, verbose=False, lazy=False):
+        """
+        Slices the N-dimensional form of the dataset based on the slice dictionary.
 
-    Returns
-    -------
-    None
-    """
-    if not isinstance(slice_dict, dict):
-        raise TypeError('slice_dict should be a dictionary of slice objects')
-    for key, val in slice_dict.items():
-        # Make sure the dimension is valid
-        if key not in self.n_dim_labels:
-            raise KeyError('Cannot slice on dimension {}.  '
-                           'Valid dimensions are {}.'.format(key, self.n_dim_labels))
-        if not isinstance(val, (slice, list, np.ndarray, tuple, int)):
-            raise TypeError('The slices must be array-likes or slice objects.')
-    return True
+        Parameters
+        ----------
+        slice_dict : dict
+            Dictionary of array-likes. for any dimension one needs to slice
+        verbose : bool, optional
+            Whether or not to print debugging statements
+        lazy : bool, optional. Default = False
+            If set to false, data_slice will be a :class:`numpy.ndarray`
+            Else returned object is :class:`dask.array.core.Array`
 
-def __slice_n_dim_form(self, slice_dict, verbose=False, lazy=False):
-    """
-    Slices the N-dimensional form of the dataset based on the slice dictionary.
-    Assumes that an N-dimensional form exists and is what was requested
-
-    Parameters
-    ----------
-    slice_dict : dict
-        Dictionary of array-likes. for any dimension one needs to slice
-    verbose : bool, optional
-        Whether or not to print debugging statements
-    lazy : bool, optional. Default = False
-        If set to false, data_slice will be a :class:`numpy.ndarray`
-        Else returned object is :class:`dask.array.core.Array`
-
-    Returns
-    -------
-    data_slice : :class:`numpy.ndarray`, or :class:`dask.array.core.Array`
-        Slice of the dataset.
-    success : bool
-        Always True
-    """
-    nd_slice = []
-
-    for dim_name in self.n_dim_labels:
-        nd_slice.append(slice_dict.get(dim_name, slice(None)))
-
-    # Dask multidimensional slicing does not work if list is passed:
-    nd_slice = tuple(nd_slice)
-    if verbose:
-        print(self.n_dim_labels)
-        print(nd_slice)
-
-    sliced_dset = self.__curr_ndim_form[nd_slice]
-    if not lazy:
-        sliced_dset = sliced_dset.compute()
-
-    return sliced_dset, True
-
-def slice(self, slice_dict, ndim_form=True, as_scalar=False, verbose=False, lazy=False):
-    """
-    Slice the dataset based on an input dictionary of 'str': slice pairs.
-    Each string should correspond to a dimension label.  The slices can be
-    array-likes or slice objects.
-
-    Parameters
-    ----------
-    slice_dict : dict
-        Dictionary of array-likes. for any dimension one needs to slice
-    ndim_form : bool, optional
-        Whether or not to return the slice in it's N-dimensional form. Default = True
-    as_scalar : bool, optional
-        Should the data be returned as scalar values only.
-    verbose : bool, optional
-        Whether or not to print debugging statements
-    lazy : bool, optional. Default = False
-        If set to false, data_slice will be a :class:`numpy.ndarray`
-        Else returned object is :class:`dask.array.core.Array`
-
-    Returns
-    -------
-    data_slice : :class:`numpy.ndarray`, or :class:`dask.array.core.Array`
-        Slice of the dataset.  Dataset has been reshaped to N-dimensions if `success` is True, only
-        by Position dimensions if `success` is 'Positions', or not reshape at all if `success`
-        is False.
-    success : str or bool
-        Informs the user as to how the data_slice has been shaped.
-
-    """
-    # TODO: Accept sequences of integers and build a list of slice objects for each dimension
-    if slice_dict is None:
-        slice_dict = dict()
-    else:
+        Returns
+        -------
+        data_slice : :class:`numpy.ndarray`, or :class:`dask.array.core.Array`
+            Slice of the dataset.
+        success : bool
+            Always True
+        """
         self.__validate_slice_dict(slice_dict)
 
-    if not isinstance(as_scalar, bool):
-        raise TypeError('as_scalar should be a bool')
-    if not isinstance(verbose, bool):
-        raise TypeError('verbose should be a bool')
+        nd_slice = []
 
-    if self.__n_dim_form_avail and ndim_form:
-        return self.__slice_n_dim_form(slice_dict, verbose=verbose, lazy=lazy)
+        for dim_name in self.n_dim_labels:
+            nd_slice.append(slice_dict.get(dim_name, slice(None)))
 
-    # Convert the slice dictionary into lists of indices for each dimension
-    pos_slice, spec_slice = self._get_pos_spec_slices(slice_dict)
-    if verbose:
-        print('Position slice: shape - {}'.format(pos_slice.shape))
-        print(pos_slice)
-        print('Spectroscopic slice: shape - {}'.format(spec_slice.shape))
-        print(spec_slice)
+        # Dask multidimensional slicing does not work if list is passed:
+        nd_slice = tuple(nd_slice)
+        if verbose:
+            print(self.n_dim_labels)
+            print(nd_slice)
 
-    # Now that the slices are built, we just need to apply them to the data
-    # This method is slow and memory intensive but shouldn't fail if multiple lists are given.
-    if lazy:
-        raw_2d = self.__lazy_2d
-    else:
-        raw_2d = self
+        sliced_dset = self.__curr_ndim_form[nd_slice]
+        if not lazy:
+            sliced_dset = sliced_dset.compute()
 
-    if verbose:
-        print('Slicing to 2D based on dataset of shape: {} and type: {}'
-              ''.format(raw_2d.shape, type(raw_2d)))
-
-    if lazy:
-        data_slice = raw_2d[pos_slice[:, 0], :][:, spec_slice[:, 0]]
-    else:
-        if len(pos_slice) <= len(spec_slice):
-            # Fewer final positions than spectra
-            data_slice = np.atleast_2d(raw_2d[pos_slice[:, 0], :])[:, spec_slice[:, 0]]
-        else:
-            # Fewer final spectral points compared to positions
-            data_slice = np.atleast_2d(raw_2d[:, spec_slice[:, 0]])[pos_slice[:, 0], :]
-
-    if verbose:
-        print('data_slice of shape: {} and type: {} after slicing'
-              ''.format(data_slice.shape, type(data_slice)))
-    if not lazy:
-        orig_shape = data_slice.shape
-        data_slice = np.atleast_2d(np.squeeze(data_slice))
-        if data_slice.shape[0] == orig_shape[1] and data_slice.shape[1] == orig_shape[0]:
-            data_slice = data_slice.T
-    if verbose:
-        print('data_slice of shape: {} after squeezing'.format(data_slice.shape))
-
-    pos_inds = self.h5_pos_inds[pos_slice, :]
-    spec_inds = self.h5_spec_inds[:, spec_slice].reshape([self.h5_spec_inds.shape[0], -1])
-    if verbose:
-        print('Sliced position indices:')
-        print(pos_inds)
-        print('Spectroscopic Indices (transposed)')
-        print(spec_inds.T)
-
-    # At this point, the empty dimensions MUST be removed in order to avoid problems with dimension sort etc.
-    def remove_singular_dims(anc_inds):
-        new_inds = []
-        for dim_values in anc_inds:
-            if len(np.unique(dim_values)) > 1:
-                new_inds.append(dim_values)
-        # if all dimensions are removed?
-        if len(new_inds) == 0:
-            new_inds = np.arange(1)
-        else:
-            new_inds = np.array(new_inds)
-        return new_inds
-
-    pos_inds = np.atleast_2d(remove_singular_dims(pos_inds.T).T)
-    spec_inds = np.atleast_2d(remove_singular_dims(spec_inds))
-
-    if verbose:
-        print('After removing any singular dimensions')
-        print('Sliced position indices:')
-        print(pos_inds)
-        print('Spectroscopic Indices (transposed)')
-        print(spec_inds.T)
-        print('data slice of shape: {}. Position indices of shape: {}, Spectroscopic indices of shape: {}'
-              '.'.format(data_slice.shape, pos_inds.shape, spec_inds.shape))
-
-    success = True
-
-    if ndim_form:
-        # TODO: if data is already loaded into memory, try to avoid I/O and slice in memory!!!!
-        data_slice, success = reshape_to_n_dims(data_slice, h5_pos=pos_inds, h5_spec=spec_inds, verbose=verbose, lazy=lazy)
-        data_slice = data_slice.squeeze()
-
-    if as_scalar:
-        return flatten_to_real(data_slice), success
-    else:
-        return data_slice, success
+        return sliced_dset
