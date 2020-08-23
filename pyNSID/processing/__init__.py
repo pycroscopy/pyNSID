4c425f71316ed5930d27fd530be340a102246d5c
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-23 14:29:20 -0400

    Copied init from master

---
+++ pyNSID/processing/__init__.py
@@ -0,0 +1,14 @@
+"""
+Formalizing data processing on NSID datasets using parallel computing tools
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: _autosummary
+
+"""
+
+#from .process import Process
+
+__all__ = []
