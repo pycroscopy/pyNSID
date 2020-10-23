1eb77afec5a8be19d4201d5b087f58f07faadbfa
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2020-10-23 16:48:04 -0400

    cleaned code deleted nsi_data.py
    in nsi_reader.py:
    - moved read_h5py_dataset to hdf_utils.py
    - added can_read
    - updated read
    - updated read
    hdf_utils.py:
    -removed functions:
    --validate_main_dimensions
    --validate_anc_h5_dsets
    --get_source_dataset
    --validate_dims_against_main
    -added and modified:
    --read_h5py_dataset

--- pyNSID/io/nsi_reader.py
+++ pyNSID/io/nsi_reader.py
@@ -10,15 +10,15 @@ from __future__ import division, print_function, absolute_import, unicode_litera
 from warnings import warn
 import sys
 import h5py
-from sidpy import Reader
+import sidpy
 
-from .hdf_utils import get_all_main, read_h5py_dataset, check_if_main
+from pyNSID.io.hdf_utils import get_all_main, read_h5py_dataset, check_if_main
 
 if sys.version_info.major == 3:
     unicode = str
 
 
-class NSIDReader(Reader):
+class NSIDReader(sidpy.Reader):
 
     def __init__(self, h5_object):
         """
@@ -60,31 +60,31 @@ class NSIDReader(Reader):
             raise TypeError('This function needs to be initialised with a hdf5 group or dataset first')
 
         if dataset is None:
-            return self.read_all_in_group(recursive=True)
+            return self.read_all(recursive=True)
         else:
             if isinstance(dataset, h5py.Dataset):
                 return read_h5py_dataset(dataset)
 
-    def read_all_in_file(self):
-        list_of_main = get_all_main(self.h5_group.file, verbose=False)
-        list_of_datasets = []
-        for dset in list_of_main:
-            list_of_datasets.append(read_h5py_dataset(dset))
-        return list_of_datasets
+    def read_all(self, recursive=True, parent=None):
 
-    def read_all_in_group(self, recursive=True):
+        if parent is None:
+            h5_group = self.h5_group
+        else:
+            if isinstance(parent, h5py.object):
+                h5_group = parent
+            else:
+                raise TypeError('parent should be a h5py object')
 
         if recursive:
-            list_of_main = get_all_main(self.h5_group, verbose=False)
+            list_of_main = get_all_main(h5_group, verbose=False)
         else:
             list_of_main = []
             for key in self.h5_group:
-                if isinstance(self.h5_group[key], h5py.Dataset):
-                    if check_if_main(self.h5_group[key]):
-                        list_of_main.append(self.h5_group[key])
+                if isinstance(h5_group[key], h5py.Dataset):
+                    if check_if_main(h5_group[key]):
+                        list_of_main.append(h5_group[key])
         # Go through each of the identified
         list_of_datasets = []
         for dset in list_of_main:
             list_of_datasets.append(read_h5py_dataset(dset))
-
         return list_of_datasets
