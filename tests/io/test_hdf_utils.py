4276c2ee7f35cf450267cb50b37da4a731efa4cb
ziatdinovmax <ziatdinovmax@gmail.com>
2020-11-06 22:03:42 +0000

    Add some tests for hdf_utils

--- tests/io/test_hdf_utils.py
+++ tests/io/test_hdf_utils.py
@@ -1,7 +1,141 @@
-from __future__ import division, print_function, unicode_literals, absolute_import
-import unittest
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import os
 import sys
+import unittest
+from typing import Tuple, Type
+
+import h5py
+import numpy as np
+
 sys.path.append("../pyNSID/")
+from pyNSID.io.hdf_io import write_nsid_dataset
+from pyNSID.io.hdf_utils import find_dataset, read_h5py_dataset
+from sidpy import Dataset, Dimension
+
+
+def create_h5group(h5f_name: str, h5g_name: str) -> Type[h5py.Group]:
+    mode = 'r+' if os.path.exists(h5f_name) else 'w'
+    h5_f = h5py.File(h5f_name, mode)
+    h5_group = h5_f.create_group(h5g_name)
+    return h5_group
+
+
+def write_dummy_dset(hf_group: Type[h5py.Group], dims: Tuple[int],
+                     main_name: str, **kwargs) -> None:
+    dnames = kwargs.get("dnames", np.arange(len(dims)))
+    dset = Dataset.from_array(
+            np.random.random([*dims]), name="new")
+    for i in range(len(dims)):
+        dset.set_dimension(
+            i, Dimension(np.arange(dset.shape[i]), "{}".format(str(dnames[i]))))
+    write_nsid_dataset(
+        dset, hf_group, main_data_name=main_name)
+
+
+def get_dset(hf_name: str, h5g_name: str, dset_name: str) -> Type[h5py.Dataset]:
+    h5group = create_h5group(hf_name, h5g_name)
+    write_dummy_dset(h5group, (10, 10, 5), dset_name)
+    hf = h5py.File(hf_name, 'r')
+    dset = find_dataset(hf, dset_name)[0]
+    return dset
+
+
+class test_read_h5py_dataset(unittest.TestCase):
+
+    def test_wrong_input_type(self) -> None:
+        self.tearDown()
+        dataset = create_h5group('test.hdf5', 'dataset')
+        err_msg = 'can only read single Dataset'
+        with self.assertRaises(TypeError) as context:
+            _ = read_h5py_dataset(dataset)
+        self.assertTrue(err_msg in str(context.exception))
+
+    def test_hdf5_info(self) -> None:
+        hf_name = "test.hdf5"
+        dset = get_dset(hf_name, "g", "d")
+        dset.attrs["title"] = "dset_name"
+        dataset = read_h5py_dataset(dset)
+        self.assertTrue(dataset.h5_filename == hf_name)
+
+    def test_attrs_title(self) -> None:
+        self.tearDown()
+        dset = get_dset("test.hdf5", "g", "d")
+        dset.attrs["title"] = "dset_name"
+        dataset = read_h5py_dataset(dset)
+        self.assertTrue(dataset.title == 'dset_name')
+
+    def test_attrs_units(self) -> None:
+        self.tearDown()
+        dset1 = get_dset("test.hdf5", "g1", "d1")
+        dset2 = get_dset("test.hdf5", "g2", "d2")
+        dset2.attrs["units"] = 'nA'
+        dataset1 = read_h5py_dataset(dset1)
+        dataset2 = read_h5py_dataset(dset2)
+        self.assertTrue(dataset1.units == 'generic')
+        self.assertTrue(dataset2.units == 'nA')
+
+    def test_attrs_quantity(self) -> None:
+        self.tearDown()
+        dset1 = get_dset("test.hdf5", "g1", "d1")
+        dset2 = get_dset("test.hdf5", "g2", "d2")
+        dset2.attrs["quantity"] = 'Current'
+        dataset1 = read_h5py_dataset(dset1)
+        dataset2 = read_h5py_dataset(dset2)
+        self.assertTrue(dataset1.quantity == 'generic')
+        self.assertTrue(dataset2.quantity == 'Current')
+
+    def test_attrs_datatype(self) -> None:
+        self.tearDown()
+        dset1 = get_dset("test.hdf5", "g1", "d1")
+        dset2 = get_dset("test.hdf5", "g2", "d2")
+        dset2.attrs["data_type"] = 'SPECTRAL_IMAGE'
+        dataset1 = read_h5py_dataset(dset1)
+        dataset2 = read_h5py_dataset(dset2)
+        self.assertTrue(dataset1.data_type.name == "UNKNOWN")
+        self.assertTrue(dataset2.data_type.name == 'SPECTRAL_IMAGE')
+
+    def test_attrs_modality(self) -> None:
+        self.tearDown()
+        dset1 = get_dset("test.hdf5", "g1", "d1")
+        dset2 = get_dset("test.hdf5", "g2", "d2")
+        dset2.attrs["modality"] = 'modality'
+        dataset1 = read_h5py_dataset(dset1)
+        dataset2 = read_h5py_dataset(dset2)
+        self.assertTrue(dataset1.modality == 'generic')
+        self.assertTrue(dataset2.modality == 'modality')
+
+    def test_attrs_source(self) -> None:
+        self.tearDown()
+        dset1 = get_dset("test.hdf5", "g1", "d1")
+        dset2 = get_dset("test.hdf5", "g2", "d2")
+        dset2.attrs["source"] = 'source'
+        dataset1 = read_h5py_dataset(dset1)
+        dataset2 = read_h5py_dataset(dset2)
+        self.assertTrue(dataset1.source == 'generic')
+        self.assertTrue(dataset2.source == 'source')
+
+    def test_dims(self) -> None:
+        self.tearDown()
+        dset = get_dset("test.hdf5", "g", "d")
+        dataset = read_h5py_dataset(dset)
+        self.assertTrue(dataset._axes[0].name == '0')
+        self.assertTrue(dataset._axes[1].name == '1')
+        self.assertTrue(dataset._axes[2].name == '2')
+
+    def test_all_attrs_inheritance(self) -> None:
+        self.tearDown()
+        dset = get_dset("test.hdf5", "g", "d")
+        dataset = read_h5py_dataset(dset)
+        self.assertTrue(all([v1 == v2 for (v1, v2) in
+                            zip(dset.attrs.values(), dataset.attrs.values())][2:]))
+        self.assertTrue(all([k1 == k2 for (k1, k2) in
+                            zip(dset.attrs.keys(), dataset.attrs.keys())]))
+
+    def tearDown(self, fname: str = 'test.hdf5') -> None:
+        if os.path.exists(fname):
+            os.remove(fname)
 
 
 class TestGetAllMain(unittest.TestCase):
