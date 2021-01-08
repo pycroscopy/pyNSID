20f8b21b0d161051cced71b208bd91466ba28c3e
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2021-01-08 15:05:27 -0500

    'nsid_version' cannot be in main_attrs either, because it is not mandatory in write either.

--- pyNSID/io/hdf_utils.py
+++ pyNSID/io/hdf_utils.py
@@ -212,7 +212,7 @@ def check_if_main(h5_main, verbose=False):
     attrs_names = ['dimension_type', 'name', 'quantity', 'units', ]
 
     # Check for all required attributes in dataset
-    main_attrs_names = ['quantity', 'units', 'main_data_name', 'nsid_version', 'data_type', 'modality', 'source']
+    main_attrs_names = ['quantity', 'units', 'main_data_name', 'data_type', 'modality', 'source']
     main_attr_success = np.all([att in h5_main.attrs for att in main_attrs_names])
     if verbose:
         print('All Attributes in dataset: ', main_attr_success)
